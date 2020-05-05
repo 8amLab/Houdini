d910a0cebbd347d2245c7edd0f6ef06ca1c56b80
TDargentina <fidel8a@gmail.com>
2020-05-04 23:22:09 -0300

    Create bulkRedshiftVolumeMaya.py

---
+++ bulkRedshiftVolumeMaya.py
@@ -0,0 +1,13 @@
+import glob, os
+import pymel.core as pm
+
+path="X:/theboat/mundo/vfx/shots/mun_02B_010/fx/publish/mun_02B_010_fx_cloudDir_v077"
+os.chdir(path)
+for file in glob.glob("*.vdb"):
+    
+    
+    volumeNode=pm.createNode( 'RedshiftVolumeShape')
+    filename="%s/%s" % (path,file)
+    volumeNode.fileName.set(filename)
+    volumeNode.displayMode.set(1)
+    volumeNode.rename(test)
